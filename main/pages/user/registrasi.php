3237fb2b18200ba665543ee70150acfb1ca42eba
Arif Hayati Indah Lestari <arifhayatiindahlestari@gmail.com>
2023-07-03 14:05:35 +0700

    add registration

---
+++ main/pages/user/registrasi.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v4.2.2
+* @link https://coreui.io
+* Copyright (c) 2022 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://coreui.io/license)
+-->
+<html lang="en">
+  <head>
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
+    <meta name="author" content="Łukasz Holeczek">
+    <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
+    <title>CoreUI Free Bootstrap Admin Template</title>
+    <link rel="manifest" href="../../lib/assets/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../../lib/assets/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Vendors styles-->
+    <link rel="stylesheet" href="../../lib/vendors/simplebar/css/simplebar.css">
+    <link rel="stylesheet" href="../../lib/css/vendors/simplebar.css">
+    <!-- Main styles for this application-->
+    <link href="../../lib/css/style.css" rel="stylesheet">
+    <!-- We use those styles to show code examples, you should remove them in your application.-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css">
+    <link href="../../lib/css/examples.css" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics-->
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
+    <style>
+      body {
+        background-image: url('../../lib/assets/img/bg3.jpg');
+        opacity: 0.9;
+        background-repeat: no-repeat;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+        background-position: center;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="min-vh-100 d-flex flex-row align-items-center">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <div class="card mb-4 mx-4">
+              <div class="card-body p-4">
+                <h1>Register</h1>
+                <p class="text-medium-emphasis">Create your account</p>
+                <div class="input-group mb-3">
+                  <span class="input-group-text">
+                    <svg class="icon">
+                      <use xlink:href="../../lib/vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                    </svg>
+                  </span>
+                  <input class="form-control" type="text" placeholder="Username">
+                </div>
+                <div class="input-group mb-3">
+                  <span class="input-group-text">
+                    <svg class="icon">
+                      <use xlink:href="../../lib/vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
+                    </svg>
+                  </span>
+                  <input class="form-control" type="text" placeholder="Email">
+                </div>
+                <div class="input-group mb-3">
+                  <span class="input-group-text">
+                    <svg class="icon">
+                      <use xlink:href="../../lib/vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+                    </svg>
+                  </span>
+                  <input class="form-control" type="password" placeholder="Password">
+                </div>
+                <div class="input-group mb-4">
+                  <span class="input-group-text">
+                    <svg class="icon">
+                      <use xlink:href="../../lib/vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+                    </svg>
+                  </span>
+                  <input class="form-control" type="password" placeholder="Repeat password">
+                </div>
+                <div class="text-center">
+                  <button class="btn btn-success btn-block " type="button">Create Account</button>
+                  <hr>
+                  <p>Sudah Punya akun? <a href="./login.php" style="text-decoration: none;color:#333333; font-weight: bold">Login</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CoreUI and necessary plugins-->
+    <script src="../../lib/vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
+    <script src="../../lib/vendors/simplebar/js/simplebar.min.js"></script>
+    <script>
+    </script>
+
+  </body>
+</html>
