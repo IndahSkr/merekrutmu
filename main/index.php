e792bf1522b69e2674efa9f01b9e641845d8ad35
Arif Hayati Indah Lestari <arifhayatiindahlestari@gmail.com>
2023-07-03 14:06:11 +0700

    update index

--- main/index.php
+++ main/index.php
@@ -1,16 +1,26 @@
+<?php
+session_start();
+if (!isset($_SESSION['upkey'])) {
+  header("location:./pages/user/login.php");
+}else{
+  $upkey = $_SESSION['upkey'];
+  $username = $_SESSION['uname'];
+  $iduser = $_SESSION['iduser'];
+  $idlevel = $_SESSION['idlevel'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <base href="./">
+  <!-- <base href="./"> -->
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-  <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
-  <meta name="author" content="Łukasz Holeczek">
+  <!-- <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
+  <meta name="author" content="Łukasz Holeczek"> -->
   <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
-  <title>CoreUI Free Bootstrap Admin Template</title>
-  <link rel="apple-touch-icon" sizes="57x57" href="lib/assets/favicon/apple-icon-57x57.png">
+  <title>Merekrutmu</title>
+  <!-- <link rel="apple-touch-icon" sizes="57x57" href="lib/assets/favicon/apple-icon-57x57.png">
   <link rel="apple-touch-icon" sizes="60x60" href="lib/assets/favicon/apple-icon-60x60.png">
   <link rel="apple-touch-icon" sizes="72x72" href="lib/assets/favicon/apple-icon-72x72.png">
   <link rel="apple-touch-icon" sizes="76x76" href="lib/assets/favicon/apple-icon-76x76.png">
@@ -23,21 +33,21 @@
   <link rel="icon" type="image/png" sizes="32x32" href="lib/assets/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="96x96" href="lib/assets/favicon/favicon-96x96.png">
   <link rel="icon" type="image/png" sizes="16x16" href="lib/assets/favicon/favicon-16x16.png">
-  <link rel="manifest" href="lib/assets/favicon/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="lib/assets/favicon/ms-icon-144x144.png">
+  <link rel="manifest" href="lib/assets/favicon/manifest.json"> -->
+  <!-- <meta name="msapplication-TileColor" content="#ffffff"> -->
+  <!-- <meta name="msapplication-TileImage" content="./lib/assets/favicon/ms-icon-144x144.png"> -->
+ 
   <meta name="theme-color" content="#ffffff">
   <!-- Vendors styles-->
-  <link rel="stylesheet" href="lib/vendors/simplebar/css/simplebar.css">
-  <link rel="stylesheet" href="lib/css/vendors/simplebar.css">
+  <link rel="stylesheet" href="./lib/vendors/simplebar/css/simplebar.css">
+  <link rel="stylesheet" href="./lib/css/vendors/simplebar.css">
   <!-- Main styles for this application-->
-  <link href="lib/css/style.css" rel="stylesheet">
+  <link href="./lib/css/style.css" rel="stylesheet">
   <!-- We use those styles to show code examples, you should remove them in your application.-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css">
-  <link href="lib/css/examples.css" rel="stylesheet">
-  <!-- Global site tag (gtag.js) - Google Analytics-->
-  <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
-  <script>
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css"> -->
+  <!-- <link href="lib/css/examples.css" rel="stylesheet"> -->
+
+  <!-- <script>
     window.dataLayer = window.dataLayer || [];
 
     function gtag() {
@@ -48,105 +58,21 @@
     gtag('config', 'UA-118965717-3');
     // Bootstrap ID
     gtag('config', 'UA-118965717-5');
-  </script>
-  <link href="lib/vendors/@coreui/chartjs/css/coreui-chartjs.css" rel="stylesheet">
+  </script> -->
+  <!-- <link href="./lib/vendors/@coreui/chartjs/css/coreui-chartjs.css" rel="stylesheet"> -->
 </head>
 
-<body>
+<body onload="theContent('dashboard')">
 
   <?php
   include "./pages/components/sidebar.php"
   ?>
 
   <div class="wrapper d-flex flex-column min-vh-100 bg-light">
-    <header class="header header-sticky mb-4">
-      <div class="container-fluid">
-        <button class="header-toggler px-md-0 me-md-3" type="button" onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()">
-          <svg class="icon icon-lg">
-            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
-          </svg>
-        </button><a class="header-brand d-md-none" href="#">
-          <svg width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="assets/brand/coreui.svg#full"></use>
-          </svg></a>
-        <ul class="header-nav d-none d-md-flex">
-          <li class="nav-item"><a class="nav-link" href="#">Dashboard</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Users</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Settings</a></li>
-        </ul>
-        <ul class="header-nav ms-auto">
-          <li class="nav-item"><a class="nav-link" href="#">
-              <svg class="icon icon-lg">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-              </svg></a></li>
-          <li class="nav-item"><a class="nav-link" href="#">
-              <svg class="icon icon-lg">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-list-rich"></use>
-              </svg></a></li>
-          <li class="nav-item"><a class="nav-link" href="#">
-              <svg class="icon icon-lg">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-              </svg></a></li>
-        </ul>
-        <ul class="header-nav ms-3">
-          <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-              <div class="avatar avatar-md"><img class="avatar-img" src="assets/img/avatars/8.jpg" alt="user@email.com"></div>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end pt-0">
-              <div class="dropdown-header bg-light py-2">
-                <div class="fw-semibold">Account</div>
-              </div><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-                </svg> Updates<span class="badge badge-sm bg-info ms-2">42</span></a><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-                </svg> Messages<span class="badge badge-sm bg-success ms-2">42</span></a><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-task"></use>
-                </svg> Tasks<span class="badge badge-sm bg-danger ms-2">42</span></a><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-comment-square"></use>
-                </svg> Comments<span class="badge badge-sm bg-warning ms-2">42</span></a>
-              <div class="dropdown-header bg-light py-2">
-                <div class="fw-semibold">Settings</div>
-              </div><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
-                </svg> Profile</a><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
-                </svg> Settings</a><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-credit-card"></use>
-                </svg> Payments<span class="badge badge-sm bg-secondary ms-2">42</span></a><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-file"></use>
-                </svg> Projects<span class="badge badge-sm bg-primary ms-2">42</span></a>
-              <div class="dropdown-divider"></div><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
-                </svg> Lock Account</a><a class="dropdown-item" href="#">
-                <svg class="icon me-2">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
-                </svg> Logout</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div class="header-divider"></div>
-      <div class="container-fluid">
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb my-0 ms-2">
-            <li class="breadcrumb-item">
-              <!-- if breadcrumb is single--><span>Home</span>
-            </li>
-            <li class="breadcrumb-item active"><span>Dashboard</span></li>
-          </ol>
-        </nav>
-      </div>
-    </header>
-    <div class="body flex-grow-1 px-3">
+    <?php include "./pages/components/header.php" ?>
+    <div class="embed-responsive" id="content-index" style="padding: 1rem">
+    </div>
+    <!-- <div class="body flex-grow-1 px-3">
       <div class="container-lg">
         <div class="row">
           <div class="col-sm-6 col-lg-3">
@@ -173,7 +99,7 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
           <div class="col-sm-6 col-lg-3">
             <div class="card mb-4 text-white bg-info">
               <div class="card-body pb-0 d-flex justify-content-between align-items-start">
@@ -198,7 +124,7 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
           <div class="col-sm-6 col-lg-3">
             <div class="card mb-4 text-white bg-warning">
               <div class="card-body pb-0 d-flex justify-content-between align-items-start">
@@ -223,7 +149,7 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
           <div class="col-sm-6 col-lg-3">
             <div class="card mb-4 text-white bg-danger">
               <div class="card-body pb-0 d-flex justify-content-between align-items-start">
@@ -248,9 +174,9 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
         </div>
-        <!-- /.row-->
+
         <div class="card mb-4">
           <div class="card-body">
             <div class="d-flex justify-content-between">
@@ -318,7 +244,7 @@
             </div>
           </div>
         </div>
-        <!-- /.card.mb-4-->
+
         <div class="row">
           <div class="col-sm-6 col-lg-4">
             <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
@@ -343,7 +269,7 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
           <div class="col-sm-6 col-lg-4">
             <div class="card mb-4" style="--cui-card-cap-bg: #00aced">
               <div class="card-header position-relative d-flex justify-content-center align-items-center">
@@ -367,7 +293,7 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
           <div class="col-sm-6 col-lg-4">
             <div class="card mb-4" style="--cui-card-cap-bg: #4875b4">
               <div class="card-header position-relative d-flex justify-content-center align-items-center">
@@ -391,9 +317,9 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
         </div>
-        <!-- /.row-->
+
         <div class="row">
           <div class="col-md-12">
             <div class="card mb-4">
@@ -407,15 +333,15 @@
                           <div class="fs-5 fw-semibold">9.123</div>
                         </div>
                       </div>
-                      <!-- /.col-->
+
                       <div class="col-6">
                         <div class="border-start border-start-4 border-start-danger px-3 mb-3"><small class="text-medium-emphasis">Recuring Clients</small>
                           <div class="fs-5 fw-semibold">22.643</div>
                         </div>
                       </div>
-                      <!-- /.col-->
+
                     </div>
-                    <!-- /.row-->
+
                     <hr class="mt-0">
                     <div class="progress-group mb-4">
                       <div class="progress-group-prepend"><span class="text-medium-emphasis small">Monday</span></div>
@@ -495,7 +421,7 @@
                       </div>
                     </div>
                   </div>
-                  <!-- /.col-->
+
                   <div class="col-sm-6">
                     <div class="row">
                       <div class="col-6">
@@ -503,15 +429,15 @@
                           <div class="fs-5 fw-semibold">78.623</div>
                         </div>
                       </div>
-                      <!-- /.col-->
+
                       <div class="col-6">
                         <div class="border-start border-start-4 border-start-success px-3 mb-3"><small class="text-medium-emphasis">Organic</small>
                           <div class="fs-5 fw-semibold">49.123</div>
                         </div>
                       </div>
-                      <!-- /.col-->
+
                     </div>
-                    <!-- /.row-->
+
                     <hr class="mt-0">
                     <div class="progress-group">
                       <div class="progress-group-header">
@@ -602,9 +528,9 @@
                       </div>
                     </div>
                   </div>
-                  <!-- /.col-->
+
                 </div>
-                <!-- /.row--><br>
+                <br>
                 <div class="table-responsive">
                   <table class="table border mb-0">
                     <thead class="table-light fw-semibold">
@@ -893,27 +819,29 @@
               </div>
             </div>
           </div>
-          <!-- /.col-->
+
         </div>
-        <!-- /.row-->
+
       </div>
-    </div>
+    </div> -->
     <footer class="footer">
       <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Bootstrap Admin Template</a> © 2022 creativeLabs.</div>
       <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a></div>
     </footer>
   </div>
   <!-- CoreUI and necessary plugins-->
-  <script src="lib/vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
-  <script src="lib/vendors/simplebar/js/simplebar.min.js"></script>
+  <script src="./lib/vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
+  <script src="./lib/vendors/simplebar/js/simplebar.min.js"></script>
   <!-- Plugins and scripts required by this view-->
-  <script src="lib/vendors/chart.js/js/chart.min.js"></script>
-  <script src="lib/vendors/@coreui/chartjs/js/coreui-chartjs.js"></script>
-  <script src="lib/vendors/@coreui/utils/js/coreui-utils.js"></script>
-  <script src="lib/js/main.js"></script>
-  <script>
-  </script>
+  <!-- <script src="./lib/vendors/chart.js/js/chart.min.js"></script> -->
+  <!-- <script src="./lib/vendors/@coreui/chartjs/js/coreui-chartjs.js"></script>
+  <script src="./lib/vendors/@coreui/utils/js/coreui-utils.js"></script> -->
+  <!-- <script src="./lib/js/main.js"></script> -->
+  <script src="./lib/js/iframe.js"></script>
 
 </body>
 
 </html>
+<?php
+}
+?>
