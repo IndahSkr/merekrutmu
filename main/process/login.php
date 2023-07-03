723de17b3a62edc8df03d837ca0c459e059507b2
Arif Hayati Indah Lestari <arifhayatiindahlestari@gmail.com>
2023-07-03 14:05:46 +0700

    add login

--- main/process/login.php
+++ main/process/login.php
@@ -29,8 +29,69 @@
 </head>
 
 <body>
+  <?php
+  session_start();
+  include "../lib/assets/curl/curl.php";
+  include "../src/alamat.php";
 
-  <script>
+  $word = $_GET['kata'];
+
+  if ($word == "login") {
+    $uname = $_POST['uname'];
+    $password = $_POST['password'];
+    $pass = hash("sha512", $password);
+
+    $dtLogin = array(
+      "kode" => "login",
+      "uname" => $uname,
+      "pass" => $pass
+    );
+
+    $dtjson = json_encode($dtLogin);
+    $send = curlpost($url, $dtjson);
+    $result = json_decode($send, TRUE);
+    $hasil = $result['status'];
+    if ($hasil > 0) {
+      $_SESSION['upkey'] = $uname . "/" . $password;
+      $_SESSION['uname'] = $result['uname'];
+      $_SESSION['iduser'] = $result['iduser'];
+      $_SESSION['idlevel'] = $result['idlevel'];
+
+      ?>
+        <script>
+          Swal.fire({
+            title: 'Sukses',
+            text: 'Login Berhasil',
+            icon: 'success',
+            timer: 1500,
+            timerProgressBar: true
+          }).then(function() {
+            window.location.href = '../'
+          })
+        </script>
+      <?php
+  
+    } else {
+    ?>
+      <script>
+        Swal.fire({
+          title: 'Gagal',
+          text: 'Username atau Password Salah',
+          text: 'Silahkan coba lagi',
+          icon: 'error',
+          timer: 1500,
+          timerProgressBar: true
+        }).then(function() {
+          window.location = '../pages/user/login.php'
+        })
+      </script>
+    <?php
+    }
+  }
+
+  ?>
+
+  <!-- <script>
     Swal.fire({
       title: 'Sukses',
       text: 'Login Berhasil',
@@ -40,7 +101,7 @@
     }).then(function() {
       window.location.href = '../'
     })
-  </script>
+  </script> -->
 
   <!-- CoreUI and necessary plugins-->
   <script src="../lib/vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
